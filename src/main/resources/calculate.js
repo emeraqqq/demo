2c53524f22672b1c43553247e6bffe220edd35e1
wushi <15317033039@163.com>
2021-08-26 18:31:47 +0800

    add daily max min

--- src/main/resources/calculate.js
+++ src/main/resources/calculate.js
@@ -4,9 +4,10 @@ function calculateAverage(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var xml = xhttp.response;
-            // var result= xml.getElementById("Double").value;
+            //var result= xml.getElementById("double").value;
             console.log(xml);
-            $('#result').append(xml);
+            //$('#result').append(xml);
+            document.getElementById("result").innerHTML=xml;
 
         }
     };
@@ -15,5 +16,61 @@ function calculateAverage(){
     var days = document.getElementById("days").value;
     var date = document.getElementById("date").value;
     xhttp.open("GET", "http://localhost:8888/stock/calculateAverage/" + symbol+"?days="+days+"&date="+date, true);
+    xhttp.send();
+}
+function calculateDailyPrice(){
+    var xhttp = new XMLHttpRequest();
+    // this part of code only executed after xhttp.send(),if backend give back 200, we process the data returned
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var xml = xhttp.response;
+            //var result= xml.getElementById("Double").value;
+            console.log(xml);
+            //$('#result').append(xml);
+            document.getElementById("result1").innerHTML=xml;
+
+        }
+    };
+    //give XMLHTTPREQUEST parameter
+    var symbol1 = document.getElementById("symbol1").value;
+    var date1 = document.getElementById("date1").value;
+    console.log(symbol1);
+    xhttp.open("GET", "http://localhost:8888/stock/calculateDailyPrice/" +symbol1+"?date="+date1,true);
+    xhttp.send();
+}
+function showMax(){
+    var xhttp = new XMLHttpRequest();
+    // this part of code only executed after xhttp.send(),if backend give back 200, we process the data returned
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var xml = xhttp.response;
+            //var result= xml.getElementById("Double").value;
+            console.log(xml);
+            //$('#result').append(xml);
+            document.getElementById("result2-1").innerHTML=xml;
+
+        }
+    };
+    //give XMLHTTPREQUEST parameter
+    var symbol2 = document.getElementById("symbol2").value;
+    xhttp.open("GET", "http://localhost:8888/stock/showMaxPrice/" +symbol2,true);
+    xhttp.send();
+}
+function showMin(){
+    var xhttp = new XMLHttpRequest();
+    // this part of code only executed after xhttp.send(),if backend give back 200, we process the data returned
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var xml = xhttp.response;
+            //var result= xml.getElementById("Double").value;
+            console.log(xml);
+            //$('#result').append(xml);
+            document.getElementById("result2-2").innerHTML=xml;
+
+        }
+    };
+    //give XMLHTTPREQUEST parameter
+    var symbol2 = document.getElementById("symbol2").value;
+    xhttp.open("GET", "http://localhost:8888/stock/showMinPrice/" +symbol2,true);
     xhttp.send();
 }
